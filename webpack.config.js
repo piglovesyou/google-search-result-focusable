54398e74793fd365920b3c737f759a2b891133f0
piglovesyou <thepiglovesyou@gmail.com>
2017-09-23 19:13:10 +0900

    Run contentscript only on Google Search pages

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,10 @@ if (fileSystem.existsSync(secretsPath)) {
 
 var options = {
   entry: {
-    popup: path.join(__dirname, "src", "js", "popup.js"),
-    options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
+    contentscript: path.join(__dirname, "src", "js", "contentscript.js"),
+    // popup: path.join(__dirname, "src", "js", "popup.js"),
+    // options: path.join(__dirname, "src", "js", "options.js"),
+    // background: path.join(__dirname, "src", "js", "background.js")
   },
   output: {
     path: path.join(__dirname, "build"),
@@ -38,11 +39,11 @@ var options = {
         loader: "file-loader?name=[name].[ext]",
         exclude: /node_modules/
       },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-        exclude: /node_modules/
-      }
+      // {
+      //   test: /\.html$/,
+      //   loader: "html-loader",
+      //   exclude: /node_modules/
+      // }
     ]
   },
   resolve: {
@@ -53,23 +54,24 @@ var options = {
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
     }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "popup.html"),
-      filename: "popup.html",
-      chunks: ["popup"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "options.html"),
-      filename: "options.html",
-      chunks: ["options"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "background.html"),
-      filename: "background.html",
-      chunks: ["background"]
-    }),
+    // new HtmlWebpackPlugin({
+    //   template: path.join(__dirname, "src", "popup.html"),
+    //   filename: "popup.html",
+    //   chunks: ["popup"]
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: path.join(__dirname, "src", "options.html"),
+    //   filename: "options.html",
+    //   chunks: ["options"]
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: path.join(__dirname, "src", "background.html"),
+    //   filename: "background.html",
+    //   chunks: ["background"]
+    // }),
     new WriteFilePlugin()
-  ]
+  ],
+  watch: true,
 };
 
 if (env.NODE_ENV === "development") {
